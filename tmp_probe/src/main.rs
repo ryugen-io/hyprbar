22b088c95ddff77369193c681a4809e208ff8514
ryu <pl@ryugen.io>
2025-12-11 21:54:58 +0100

    docs: add written workflow explanation to ARCHITECTURE.md

--- tmp_probe/src/main.rs
+++
@@ -1,7 +0,0 @@
-use ks_core::state::BarState;
-
-fn main() {
-    let state: BarState = unsafe { std::mem::zeroed() };
-    // Trigger error to see available fields or confirm existence
-    let _ = state.cookbook.icons; 
-}
